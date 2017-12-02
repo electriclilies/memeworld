922c5d9d1b930f942744a15d2319318322c9fae3
Minkee73 <34142645+Minkee73@users.noreply.github.com>
2017-12-02 13:09:39 -0500

    Add files via upload

---
+++ GameCharacter.java
@@ -0,0 +1,101 @@
+/**
+ * GameCharacter.java
+ * @author Emma Shumadine
+ * */
+
+public class GameCharacter {
+  protected int currentHP;
+  protected int maxHP;
+  protected Weapon weapon;
+  
+  /**
+   * Creates a new game character
+   * @param hp the maximum hp of the character
+   * @param w the weapon of the character
+   * */
+  public GameCharacter(int hp, Weapon w) {
+    maxHP = hp;
+    currentHP = maxHP;
+    weapon = w;
+  }
+  
+  /**
+   * Attacks the target with the weapon. Returns -1 if the attack misses, and the damage of the attack otherwise.
+   * @param target the target of the attack
+   * @return -1 if the attack misses, and the damage of the attack otherwise
+   * */
+  public int attack(GameCharacter target) {
+    int attackResult = weapon.use();
+    if(attackResult != -1) {
+      target.changeHP(-attackResult);
+    }
+    return attackResult;
+  }
+  
+  /**
+   * Returns the maximum HP of the character
+   * @return the maximum hp
+   * */
+  public int getMaxHP() {
+    return maxHP;
+  }
+  
+  /**
+   * Returns the current HP of the character
+   * @return the current hp
+   * */
+  public int getCurrentHP() {
+    return currentHP;
+  }
+  
+  /**
+   * Returns the character's weapon
+   * @return the weapon
+   * */
+  public Weapon getWeapon() {
+    return weapon;
+  }
+  
+  /**
+   * Sets the maximum HP of the character
+   * @param the maximum hp
+   * */
+  public void setMaxHP(int hp) {
+    maxHP = hp;
+  }
+  
+  /**
+   * Changes the current HP of the character
+   * @param amount the amount to change the hp by
+   * */
+  public void changeHP(int amount) {
+    int total = currentHP + amount;
+    if(total > maxHP) {
+      currentHP = maxHP;
+    } else if (total < 0) {
+      currentHP = 0;
+    } else {
+      currentHP += amount;
+    }
+  }
+  
+  /**
+   * Sets the current HP of the character
+   * @param hp the hp
+   * */
+  public void setHP(int hp) {
+    if(hp > maxHP) {
+      currentHP = maxHP;
+    } else {
+      currentHP = hp;
+    }
+  }
+  
+  /**
+   * Sets the character's weapon
+   * @param w the weapon
+   * */
+  public void setWeapon(Weapon w) {
+    weapon = w;
+  }
+}
---
+++ Weapon.java
@@ -0,0 +1,70 @@
+/**
+ * Weapon.java
+ * @author Emma Shumadine
+ * */
+
+import java.util.Random;
+
+public class Weapon extends Item {
+  private int hitRate;
+  private Dice damageDice;
+  private static Random toRoll = new Random();
+  
+  /**
+   * Creates a weapon
+   * @param n the name
+   * @param desc the description
+   * @param hR the hit rate
+   * @param dD the damage string
+   * */
+  public Weapon(String n, String desc, double hR, String dD) {
+    super(n, desc);
+    hitRate = (int)(100*hR);
+    damageDice = new Dice(dD);
+  }
+  
+  /**
+   * Uses the weapon. Returns -1 if the weapon misses and the weapon's damage otherwise.
+   * @return -1 if the weapon misses and the weapon's damage otherwise
+   * */
+  public int use() {
+    int hit = toRoll.nextInt();
+    if(hit < hitRate) {
+      return damageDice.roll();
+    } else {
+      return -1;
+    }
+  }
+  
+  /**
+   * Returns the weapon's hit rate
+   * @return the hit rate
+   * */
+  public double getHitRate() {
+    return Integer.valueOf(hitRate).doubleValue()/100;
+  }
+  
+  /**
+   * Returns the weapon's damage dice
+   * @return the damage dice
+   * */
+  public Dice getDamage() {
+    return damageDice;
+  }
+  
+  /**
+   * Sets the hit rate of the weapon
+   * @param hR the hit rate
+   * */
+  public void setHitRate(double hR) {
+    hitRate = (int)(100*hR);
+  }
+  
+  /**
+   * Sets the damage of the weapon
+   * @param dD the damage string
+   * */
+  public void setDamage(String dD) {
+    damageDice = new Dice(dD);
+  }
+}
