705187356272d3aa1d1d8df425bea4b622da4570
electriclilies <lilyorthsmith@gmail.com>
2017-12-09 17:40:03 -0500

    Add files via upload

---
+++ IllegalMoveException.java
@@ -0,0 +1,13 @@
+/* An execption for when someone tries to make an illegal move
+ * Author: Lily Orth-Smith
+ * Date: Dec 9
+ */
+
+public class IllegalMoveException extends RuntimeException {
+
+  public IllegalMoveException ()
+  {
+    super();
+  }
+  
+}
